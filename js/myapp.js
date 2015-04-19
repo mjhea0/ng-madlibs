5e16e93706e88e38e749a3bcade8a9044cc2ab4c
Caitlin <caitlin.colbert@gmail.com>
2015-04-19 11:37:30 -0700

    adding reset button

--- js/myapp.js
+++ js/myapp.js
@@ -7,20 +7,22 @@ angular.module('madLibs', [])
     .controller('myCtrl', function($scope) {
         $scope.submit = function() {
             if ($scope.inputForm.$valid) {
-                console.log("valid");
                 $scope.result = 1;
                 console.log($scope.result);
-                $scope.template = {
-                    name: 'name',
-                    job_title: 'job title',
-                    tedious_task: 'tedious task',
-                    dirty_task: 'dirty task',
-                    celebrity: 'celebrity',
-                    useless_skill: 'useless skill',
-                    obnoxious_celebrity: 'obnoxious celebrity',
-                    huge_number: 'huge number',
-                    adjective: 'adjective'
-                };
             }
         };
+        $scope.reset = function() {
+            $scope.result = 0;
+            $scope.name = null;
+            $scope.job_title = null;
+            $scope.tedious_task = null;
+            $scope.dirty_task = null;
+            $scope.celebrity = null;
+            $scope.huge_number = null;
+            $scope.useless_skill = null;
+            $scope.obnoxious_celebrity = null;
+            $scope.adjective = null;
+        };
+
+        $scope.result = 0;
     });
