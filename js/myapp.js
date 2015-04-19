28e8113829fd92da326decf65cc0ffc14185ca67
Caitlin <caitlin.colbert@gmail.com>
2015-04-18 18:11:02 -0700

    adding ng-show and hide

--- js/myapp.js
+++ js/myapp.js
@@ -1,14 +1,26 @@
+$(document).ready(function () {
+    console.log("dom is ready!");
+    
+});
+
 angular.module('madLibs', [])
     .controller('myCtrl', function($scope) {
-        $scope.template = [{
-            name: 'name',
-            job_title: 'job title',
-            tedious_task: 'tedious task',
-            dirty_task: 'dirty task',
-            celebrity: 'celebrity',
-            useless_skill: 'useless skill',
-            obnoxious_celebrity: 'obnoxious celebrity',
-            huge_number: 'huge number',
-            adjective: 'adjective'
-        }];
+        $scope.submit = function() {
+            if ($scope.inputForm.$valid) {
+                console.log("valid");
+                $scope.result = 1;
+                console.log($scope.result);
+                $scope.template = {
+                    name: 'name',
+                    job_title: 'job title',
+                    tedious_task: 'tedious task',
+                    dirty_task: 'dirty task',
+                    celebrity: 'celebrity',
+                    useless_skill: 'useless skill',
+                    obnoxious_celebrity: 'obnoxious celebrity',
+                    huge_number: 'huge number',
+                    adjective: 'adjective'
+                };
+            }
+        };
     });
